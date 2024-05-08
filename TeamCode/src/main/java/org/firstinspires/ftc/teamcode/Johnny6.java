29ad035129d4af15583c94d38efaecffbe696781
ethanvader <93680550+ethanvader@users.noreply.github.com>
2024-05-08 17:19:05 -0400

    Created more stuff for Johnny6

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Johnny6.java
@@ -0,0 +1,120 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
+public class Johnny6 {
+
+    private HardwareMap hwMap;
+
+    LinearOpMode auton;
+
+    public enum Drivetrain {
+        MECHANUM,
+        JOHNNY6,
+        TEST
+    }
+
+    public enum Team {
+        RED,
+        BLUE
+    }
+
+    private Drivetrain drive;
+
+    private Telemetry telem;
+
+    //Definitions for global variables
+
+    public DcMotor motorFrontLeft, motorFrontRight, motorBackLeft, motorBackRight;
+    //[] means array
+    public DcMotor[] allDriveMotors;
+
+    //public DcMotor //future necessary robot functions using motors
+
+    //public CRServo //future necessary robot functions using servos
+    private IMU imu;
+
+    private IMU.Parameters parameters;
+
+    public WebcamName eyeofjohnny6;
+
+    //Put any CONSTANTS here
+
+    static final double Y_INCH_TICKS = 40;
+
+    static final double X_INCH_TICKS = 40;
+
+    public Johnny6 (OpMode opmode, Drivetrain drivetrain) {
+
+        this.hwMap = opmode.hardwareMap;
+
+        this.drive = drivetrain;
+
+        this.telem = opmode.telemetry;
+
+        //setUpHardware maps variables to their hardware object
+        setupHardware();
+    }
+
+    public Johnny6(LinearOpMode opmode, Drivetrain type) {
+
+        this.auton = opmode;
+
+        hwMap = opmode.hardwareMap;
+
+        telem = opmode.telemetry;
+
+        drive = type;
+
+        setupHardware();
+    }
+
+    public Johnny6(HardwareMap hardwareMap, Drivetrain drivetrain) {
+
+        this.hwMap = hardwareMap;
+
+        this.drive = drivetrain;
+
+        setupHardware();
+    }
+
+    private void setupHardware() {
+
+        //This switch statement is used to choose which drivetrain
+        //depending on the drive variable
+        switch ( drive ) {
+
+            case JOHNNY6:
+
+                motorFrontLeft = hwMap.dcMotor.get("motorFrontLeft");
+                motorFrontRight = hwMap.dcMotor.get("motorFrontRight");
+                motorBackLeft = hwMap.dcMotor.get("motorBackLeft");
+                motorBackRight = hwMap.dcMotor.get("motorBackRight");
+
+
+
+                motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+                imu=hwMap.get(IMU.class,"imu");
+
+
+
+                parameters=new IMU.Parameters(new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
+                        RevHubOrientationOnRobot.UsbFacingDirection.LEFT));
+
+
+                imu.initialize(parameters);
+        }
+    }
+}
